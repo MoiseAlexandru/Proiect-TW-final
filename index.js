d906fdfc939a7a8c541a0feb8888b5ecbfaee59a
MoiseAlexandru <moise.alexandru.ichb@gmail.com>
2022-04-12 15:02:45 +0300

    trecut pe node + instalat ejs

--- index.js
+++ index.js
@@ -2,8 +2,25 @@ const express = require("express");
 
 app = express(); /// server numit app
 
-app.get("/ceva", (req, res) => {
-    res.write("salut");
+app.use("/resurse", express.static(__dirname + "/resurse"));
+
+app.get("/", (req, res) => {
+    console.log(__dirname);
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.get("/ceva", (req, res, next) => {
+    res.write("salut1");
+    next();
+    //res.end();
+})
+app.get("/ceva", (req, res, next) => {
+    res.write("salut2");
+    next();
+})
+
+app.get("/*", (req, res) => {
+    res.write("cerere generala");
     res.end();
 })
 
