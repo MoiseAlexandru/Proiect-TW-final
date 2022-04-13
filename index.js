9375d24a6b721cef5336c92d19912f553f8e7f37
MoiseAlexandru <moise.alexandru.ichb@gmail.com>
2022-04-13 22:07:10 +0300

    terminat etapa 3

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ app.set("view engine", "ejs");
 app.use("/resurse", express.static(__dirname + "/resurse"));
 
 app.get(["/", "/index", "/home"], (req, res) => {
-    console.log(__dirname);
-    res.render("pagini/index");
+    console.log("pagini" + "/index");
+    res.render("pagini/index", {ip: req.ip});
+})
+
+
+app.get("/*.ejs", (req, res) => {
+    res.status(403).render("pagini/403.ejs");
 })
 
 app.get("/*", (req, res) => {
     console.log("pagini" + req.url);
-    res.render("pagini" + req.url);
+    res.render("pagini" + req.url, (err, rezRender) => {
+        if(err) {
+            if(err.message.includes("Failed to lookup view")) {
+                console.log(err);
+                res.status(404).render("pagini/404");
+            }
+            else {
+                res.render("pagini/eroare_generala");
+            }
+        }
+        else {
+            console.log("Pagina se incarca");
+            res.send(rezRender);
+        }
+    });
     res.end();
 })
 
