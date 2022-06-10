00c33b575f20537cc63ee3ff1d2e9e45c3767c79
unknown <moise.alexandru.ichb@gmail.com>
2022-06-10 04:52:47 +0300

    adaugat imagini galerie statica

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const sharp = require("sharp");
 
 app = express(); /// server numit app
 
@@ -7,7 +9,8 @@ app.use("/resurse", express.static(__dirname + "/resurse"));
 
 app.get(["/", "/index", "/home"], (req, res) => {
     console.log("pagini" + "/index");
-    res.render("pagini/index", {ip: req.ip});
+    //console.log(obImages);
+    res.render("pagini/index", {ip: req.ip, images: obImages});
 })
 
 app.get("/about", (req, res) => {
@@ -16,12 +19,12 @@ app.get("/about", (req, res) => {
 })
 
 app.get("/*.ejs", (req, res) => {
-    res.status(403).render("pagini/403.ejs");
+    res.status(403).render("pagini/403");
 })
 
 app.get("/*", (req, res) => {
     console.log("pagini" + req.url);
-    res.render("pagini" + req.url, (err, rezRender) => {
+    res.render("pagini" + req.url, {images: obImages}, (err, rezRender) => {
         if(err) {
             if(err.message.includes("Failed to lookup view")) {
                 console.log(err);
@@ -38,6 +41,39 @@ app.get("/*", (req, res) => {
     });
     res.end();
 })
+
+
+function createImages() {
+    
+    var buf = fs.readFileSync(__dirname + "/resurse/json/gallery.json").toString("utf8");
+    
+    obImages = JSON.parse(buf);
+    
+    for(let image of obImages.images) {
+        let imageName, extension;
+        [imageName, extension] = image.file.split(".");
+        
+        image.og = `${obImages.gallery_path}/${image.file}`;
+
+        let dim_small = 150;
+        image.small = `${obImages.gallery_path}/small/${imageName}-${dim_small}.webp`;
+        if(!fs.existsSync(image.small))
+            sharp(__dirname + "/" + image.og).resize(dim_small, dim_small, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0.5}}).toFile(__dirname + "/" + image.small);
+        
+        let dim_medium = 350;
+        image.medium = `${obImages.gallery_path}/medium/${imageName}-${dim_medium}.png`;
+        if(!fs.existsSync(image.medium))
+            sharp(__dirname + "/" + image.og).resize(dim_medium, dim_medium, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0.5}}).toFile(__dirname + "/" + image.medium);
+
+        let dim_big = 700;
+        image.big = `${obImages.gallery_path}/big/${imageName}-${dim_big}.png`;
+        if(!fs.existsSync(image.big))
+            sharp(__dirname + "/" + image.og).resize(dim_big, dim_big, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0.5}}).toFile(__dirname + "/" + image.big);
+    }
+    
+}
+
+createImages();
 
 app.listen(8080);
 console.log("Server is running!");
