fe9960b160b4fa5143e2bf5a8ec813ca0e398663
unknown <moise.alexandru.ichb@gmail.com>
2022-06-15 07:02:55 +0300

    terminat galerie animata

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 const express = require("express");
 const fs = require("fs");
 const sharp = require("sharp");
+const ejs = require("ejs");
+const sass = require("sass");
+
 
 app = express(); /// server numit app
 
 app.set("view engine", "ejs");
 app.use("/resurse", express.static(__dirname + "/resurse"));
 
+let nrImaginiSlideshow = 0;
+app.get("*/galerie-animata.css", (req, res) => {
+    
+    var sirScss = fs.readFileSync(__dirname + "/resurse/scss/galerie-animata.scss").toString("utf8");
+    rezScss = ejs.render(sirScss, {nrImagini:nrImaginiSlideshow});
+    console.log(nrImaginiSlideshow);
+    var scssPath = __dirname + "/temp/galerie-animata.scss";
+    fs.writeFileSync(scssPath, rezScss);
+    
+    try {
+        compileResult = sass.compile(scssPath, {sourceMap: true});
+        fs.writeFileSync(__dirname + "/temp/galerie-animata.css", compileResult.css);
+        res.setHeader("Content-Type", "text/css");
+        res.sendFile(__dirname + "/temp/galerie-animata.css")
+    }
+    catch(err) {
+        console.log(err);
+        res.send("Eroare de compilare css/scss/sass");
+    }
+})
+
 app.get(["/", "/index", "/home"], (req, res) => {
     console.log("pagini" + "/index");
     //console.log(obImages);
-    res.render("pagini/index", {ip: req.ip, images: obImages});
+    nrImaginiSlideshow = Math.floor(Math.random() * 4) * 2 + 6;  /// (0, 1, 2, 3) * 2 la care adaug 6 -> (6, 8, 10, 12) imagini
+    var imagesSlideshow = [];
+    for(let image of obImages.images) {
+        if(image.properties.includes("figurine-sale"))
+            imagesSlideshow.push(image);
+    }
+    shuffleArray(imagesSlideshow);
+    var shuffled = [];
+    for(let i = 0; i < nrImaginiSlideshow; i++)
+        shuffled.push(imagesSlideshow[i]);
+    res.render("pagini/index", {ip: req.ip, imagesSlideshow: shuffled});
 })
 
 app.get("/about", (req, res) => {
@@ -30,11 +64,7 @@ app.get("/produse", (req, res) => {
     for(let image of obImages.images) {
         if(products.length == 12)
             break;
-        let keep = false;
-        for(let property of image.properties)
-            if(property == "figurine-sale")
-                keep = true;
-        if(keep == true)
+        if(image.properties.includes("figurine-sale"))
             products.push(image);
     }
     res.render("pagini/produse", {images: products}, (err, rezRender) => {
@@ -91,19 +121,28 @@ function createImages() {
         let dim_small = 350;
         image.small = `${obImages.gallery_path}/small/${imageName}-${dim_small}.webp`;
         if(!fs.existsSync(image.small))
-            sharp(__dirname + "/" + image.og).resize(dim_small, dim_small, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0.5}}).toFile(__dirname + "/" + image.small);
+            sharp(__dirname + "/" + image.og).resize(dim_small, dim_small, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 1}}).toFile(__dirname + "/" + image.small);
         
         let dim_medium = 450;
         image.medium = `${obImages.gallery_path}/medium/${imageName}-${dim_medium}.png`;
         if(!fs.existsSync(image.medium))
-            sharp(__dirname + "/" + image.og).resize(dim_medium, dim_medium, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0.5}}).toFile(__dirname + "/" + image.medium);
+            sharp(__dirname + "/" + image.og).resize(dim_medium, dim_medium, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 1}}).toFile(__dirname + "/" + image.medium);
 
         let dim_big = 700;
         image.big = `${obImages.gallery_path}/big/${imageName}-${dim_big}.png`;
         if(!fs.existsSync(image.big))
-            sharp(__dirname + "/" + image.og).resize(dim_big, dim_big, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 0.5}}).toFile(__dirname + "/" + image.big);
+            sharp(__dirname + "/" + image.og).resize(dim_big, dim_big, {fit: 'contain', background: { r: 255, g: 255, b: 255, alpha: 1}}).toFile(__dirname + "/" + image.big);
     }
     
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
 
 createImages();
